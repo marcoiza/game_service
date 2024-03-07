dde02d51bb5b55ec0f80de0397e3201413deada4
Marco Iza <maiza4@espe.edu.ec>
2024-03-07 07:38:31 -0500

    config main

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from db.routers import user_routes
 
 app = FastAPI()
+
+# Configuración del middleware CORS para permitir cualquier acceso
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permite cualquier origen (puedes especificar dominios permitidos en lugar de "*")
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite cualquier método HTTP
+    allow_headers=["*"],  # Permite cualquier encabezado HTTP
+)
 
 app.include_router(user_routes.router)
 
 @app.get("/")
 async def root():
-  return {"Hello": "World"}
+    return {"Hello": "World"}
